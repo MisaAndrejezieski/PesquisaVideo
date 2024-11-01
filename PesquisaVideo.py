b859a595b447866914989fa5624d2ce732e126d3
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-11-01 14:46:09 -0300

    Update PesquisaVideo.py

--- PesquisaVideo.py
+++ PesquisaVideo.py
@@ -12,7 +12,7 @@ logging.basicConfig(
     encoding='utf-8'
 )
 
-# Lista de vídeos do canal (URL dos vídeos), cade meu codigo
+# Lista de vídeos do canal (URL dos vídeos)
 videos = [
     'https://youtu.be/Ed8OnrNFWe0'
     # Adicione mais URLs dos vídeos aqui
